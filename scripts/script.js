3b9dbefb1a5dcc9938b6bba8a2f165d46f44f54c
Piero David <piero_1997_12@hotmail.com>
2022-01-09 11:12:37 -0500

    animations

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,9 @@ const white_left = document.querySelector(".white-left");
 const white_right = document.querySelector(".white-right");
 const white_top = document.querySelector(".white-top");
 
+const pag_nos = document.querySelector(".nosotros");
+const pag_ser = document.querySelector(".servicios");
+
 const nav = document.querySelector(".nav-container");
 
 inicio.addEventListener("click", () =>{
@@ -27,6 +30,13 @@ nosotros.addEventListener("click", () =>{
     white_left.style.animation = "wl_left 2s normal both";
     white_right.style.animation = "wr_left 2s normal both";
     white_top.style.animation = "w_top 1.5s normal both";
+
+    pag_ser.style.animation = "desaparition 1s normal both";
+    pag_ser.style.display = "none";
+
+    pag_nos.style.display = "block";
+    pag_nos.style.animation = "aparition 2s normal both";
+
     nav.style.animation = "navbot 2s normal both";
 });
   
@@ -36,6 +46,13 @@ servicios.addEventListener("click", () =>{
     white_left.style.animation = "wl_right 2s normal both";
     white_right.style.animation = "wr_right 2s normal both";
     white_top.style.animation = "w_top 1.5s normal both";
+
+    pag_nos.style.animation = "desaparition 1s normal both";
+    pag_nos.style.display = "none";
+    
+    pag_ser.style.display = "block";
+    pag_ser.style.animation = "aparition 2s normal both";
+
     nav.style.animation = "navbot 2s normal both";
 });  
   
@@ -45,5 +62,8 @@ contactanos.addEventListener("click", () =>{
     white_left.style.animation = "wl_left 2s normal both";
     white_right.style.animation = "wr_right 2s normal both";
     white_top.style.animation = "w_bot 2.5s normal both";
+
+    pag_nos.style.display = "none";
+
     nav.style.animation = "navbot 2s normal both";
 });
